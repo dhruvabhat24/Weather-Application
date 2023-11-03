993fb6e2d86680247367b0b4c1085fe214fa7615
Dhruvabhat24 <dhruvabhat24@gmail.com>
2023-11-03 11:41:25 +0530

    all js parts are applied

--- Weather-Application/assets/js/api.js
+++ Weather-Application/assets/js/api.js
@@ -8,8 +8,39 @@
 
 const api_key ="e4d474a322c0877f50ad1ce9bfa13d83";
 
-export const fetchData = function(URL, callback) {
-    fetch('${URL}&appid=${api_key}')
-    .then(res => res.json())
-    .then(data => callback(data));
-}
+/**
+ *
+ * @param {string} URL API URL
+ * @param {Function} callback callback
+ */
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+      .then((res) => res.json())
+      .then((data) => callback(data));
+  };
+  
+  export const url = {
+    currentWeather(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=imperial`;
+    },
+    forecast(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=imperial`;
+    },
+    airPollution(lat, lon) {
+      return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+    },
+    reverseGeo(lat, lon) {
+      return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    },
+  
+    /**
+     *
+     * @param {string} query Search Query E.G.: "London", "Philadelphia"
+     * @returns
+     */
+  
+    geo(query) {
+      return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    },
+  };
--- Weather-Application/assets/js/route.js
+++ Weather-Application/assets/js/route.js
@@ -4,3 +4,52 @@
  * @copyright Dhruva 2023 All rights reserved
  * @author Dhruva <dhruvabhat24@gmail.com>
  */
+"use strict";
+
+import { updateWeather, error404 } from "./app.js";
+const defaultLocation = "#/weather?lat=40.0932222&lon=-74.9627553";
+
+const currentLocation = function () {
+  window.navigator.geolocation.getCurrentPosition(
+    (res) => {
+      const { latitude, longitude } = res.coords;
+
+      updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    },
+    (err) => {
+      window.location.hash = defaultLocation;
+    }
+  );
+};
+
+/**
+ *
+ * @param {string} query Searched query
+ */
+const searchedLocation = (query) => updateWeather(...query.split("&"));
+// updateWeather("lat=51.5073219", "lon=-0.1276474")
+
+const routes = new Map([
+  ["/current-location", currentLocation],
+  ["/weather", searchedLocation],
+]);
+
+const checkHash = function () {
+  const requestURL = window.location.hash.slice(1);
+
+  const [route, query] = requestURL.includes
+    ? requestURL.split("?")
+    : [requestURL];
+
+  routes.get(route) ? routes.get(route)(query) : error404();
+};
+
+window.addEventListener("hashchange", checkHash);
+
+window.addEventListener("load", function () {
+  if (!window.location.hash) {
+    window.location.hash = "#/current-location";
+  } else {
+    checkHash();
+  }
+});
